63fa85a71f3f09f6afe6964719accf10d6178e62
Sparsh <sparshgoelk@gmail.com>
2024-11-14 23:14:34 +0530

    Added new css styles

--- src/components/AppDownload/AppDownload.jsx
+++ src/components/AppDownload/AppDownload.jsx
@@ -1,14 +1,18 @@
-import { assets } from '../../assets/assets';
+import React from 'react';
 import './AppDownload.css';
+import { assets } from '../../assets/assets';
+
 const AppDownload = () => {
   return (
     <div className="app-download" id="app-download">
-      <img src={assets.banner} alt="" />
-      <span>
-        For Better Experience <br />
-        Download <br />
-        Artisan Cup App
-      </span>
+      <p>
+        For Better Exprience Download <br />
+        Tomato App
+      </p>
+      <div className="app-download-platforms">
+        <img src={assets.play_store} alt="" />
+        <img src={assets.app_store} alt="" />
+      </div>
     </div>
   );
 };
